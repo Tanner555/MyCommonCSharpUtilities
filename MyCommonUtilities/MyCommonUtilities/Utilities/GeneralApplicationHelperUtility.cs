6283d9337ad650642e7e5266611a52b480072775
Tanner555 <theman989898@gmail.com>
2018-10-30 07:10:21 -0700

    Added MsBuildHelper, GeneralAppHelper and SimpleFileParser Utilities
    Some Methods Come From USharp's Plugin Installer

---
+++ MyCommonUtilities/MyCommonUtilities/Utilities/GeneralApplicationHelperUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCommonUtilities
+{
+    class GeneralApplicationHelperUtility
+    {
+        public static string[] ParseArgs(string commandLine)
+        {
+            char[] parmChars = commandLine.ToCharArray();
+            bool inSingleQuote = false;
+            bool inDoubleQuote = false;
+            for (var index = 0; index < parmChars.Length; index++)
+            {
+                if (parmChars[index] == '"' && !inSingleQuote)
+                {
+                    inDoubleQuote = !inDoubleQuote;
+                    parmChars[index] = '\n';
+                }
+                if (parmChars[index] == '\'' && !inDoubleQuote)
+                {
+                    inSingleQuote = !inSingleQuote;
+                    parmChars[index] = '\n';
+                }
+                if (!inSingleQuote && !inDoubleQuote && parmChars[index] == ' ')
+                {
+                    parmChars[index] = '\n';
+                }
+            }
+            return (new string(parmChars)).Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
