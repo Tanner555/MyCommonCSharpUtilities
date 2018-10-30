2368607829ce274a098825199fe2504a12abf8e1
Tanner555 <theman989898@gmail.com>
2018-10-30 06:44:16 -0700

    Initial Commit

---
+++ MyCommonUtilities/MyCommonUtilities/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCommonUtilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From Common Utilities");
+            Console.ReadLine();
+        }
+    }
+}
