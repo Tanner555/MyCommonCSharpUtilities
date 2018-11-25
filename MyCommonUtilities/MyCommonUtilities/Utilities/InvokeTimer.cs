6247bf26ded2192cbe18ee1255492eacb0fcf190
Tanner555 <theman989898@gmail.com>
2018-11-25 11:37:17 -0800

    Added Invoke Timer

---
+++ MyCommonUtilities/MyCommonUtilities/Utilities/InvokeTimer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MyCommonUtilities
+{
+    class InvokeTimer
+    {
+        /// <summary>
+        /// May Be Issues If Invoke Is Called With Another Callback Before Timer is Elapsed
+        /// </summary>
+        static Timer onceInvokeTimer = null;
+
+        static Dictionary<Action, Timer> ActionTimerDictionary = new Dictionary<Action, Timer>();
+
+        public static void Invoke(float delay, Action callback)
+        {
+            if (onceInvokeTimer != null)
+            {
+                onceInvokeTimer.Stop();
+                onceInvokeTimer.Dispose();
+            }
+
+            onceInvokeTimer = new System.Timers.Timer();
+            onceInvokeTimer.Interval = delay;
+            onceInvokeTimer.AutoReset = false;
+            onceInvokeTimer.Elapsed += (obj, elapsedArgs) =>
+            {
+                onceInvokeTimer.Stop();
+                callback();
+                onceInvokeTimer.Dispose();
+            };
+            onceInvokeTimer.Start();
+        }
+
+        public static void InvokeRepeating(float delay, float interval, Action callback)
+        {
+            if (ActionTimerDictionary.ContainsKey(callback))
+            {
+                var _oldtimer = ActionTimerDictionary[callback];
+                _oldtimer.Stop();
+                _oldtimer.Dispose();
+                ActionTimerDictionary.Remove(callback);
+            }
+            Timer newTimer = new Timer();
+            newTimer.Interval = interval;
+            newTimer.AutoReset = true;
+            ActionTimerDictionary.Add(callback, newTimer);
+            newTimer.Start();
+        }
+
+        public static void CancelInvoke()
+        {
+            foreach (Timer timer in ActionTimerDictionary.Values)
+            {
+                timer.Stop();
+                timer.Dispose();
+            }
+            ActionTimerDictionary = new Dictionary<Action, Timer>();
+        }
+    }
+}
