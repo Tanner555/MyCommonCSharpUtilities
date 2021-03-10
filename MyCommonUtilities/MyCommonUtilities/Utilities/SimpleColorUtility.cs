f9d07f7d19f0bc2c69de7ae0d1c5fc4fde88ed42
Tanner555 <theman989898@gmail.com>
2021-03-10 15:44:31 -0800

    Tried Adding SimpleColorUtility Class

---
+++ MyCommonUtilities/MyCommonUtilities/Utilities/SimpleColorUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCommonUtilities
+{
+    public class SimpleColorUtility
+    {
+        System.Random _myRandom;
+
+        public SimpleColorUtility(System.Random _random = null)
+        {
+            _myRandom = _random != null ? _random : new System.Random();
+        }
+
+        //public System.Drawing.Color GetRandomColor()
+        //{
+        //    return System.Drawing.Color.FromArgb(_myRandom.Next(0, 255), _myRandom.Next(0, 255), _myRandom.Next(0, 255));
+        //}
+    }
+}
